86db0a22290ec9f6d5aadb551f0afadbe0c88536
Wes Parish <wes@cowtownt.org>
2016-01-18 17:48:33 -0600

    Initial creation of contact us (working) and user login/logout (working)

--- ConvGCodeGenerator/urls.py
+++ ConvGCodeGenerator/urls.py
@@ -1,7 +1,11 @@
-from django.conf.urls import url
 
 from . import views
+from django.conf.urls import url, include
 
 urlpatterns = [
+    url(r'^contact/$', views.contact_us, name='contact_us'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/cg2'}, name='auth_logout'),
+    url(r'^logout/(?P<next_page>.*)/$', 'django.contrib.auth.views.logout', name='auth_logout_next'),
+    url('^', include('django.contrib.auth.urls')),  
     url(r'^$', views.index, name='index'),
 ]
--- ConvGCodeGenerator/views.py
+++ ConvGCodeGenerator/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
-from django.template.loader import get_template
 
 # Create your views here.
 def index(request):
-    link_list = ['Machines', 
-                 'Operations', 
-                 'Post Processors']
     machines = [("Lathe", 'enabled'),
                   ("Mill", 'disabled'),
                   ("EDM", 'disabled'),
@@ -26,8 +21,37 @@ def index(request):
     
     context = {'machines': machines, 
                'operations': operations, 
-               'post_processors': post_processors}
-
-    #print(get_template("/ConvGCodeGenerator/index.html"))
+               'post_processors': post_processors,
+               'user': request.user}
     
     return render(request, 'ConvGCodeGenerator/index.html', context)
+
+def contact_us(request):
+    if request.method == 'POST':
+        import smtplib
+        from time import ctime
+        from email.mime.text import MIMEText
+        
+        context = {'alert': {}}
+        try:
+            msg = MIMEText("""
+    Date: """ + ctime() + """
+    From: """ + request.POST['name'] + """
+    Email: """ + request.POST['email'] + """
+    \n\n""" + request.POST['comment'])
+            msg['Subject'] = 'New comment email from CG2 website'
+            msg['From'] = request.POST['email']
+            msg['To'] = 'wes@firstshotprecision.com'
+    
+            s = smtplib.SMTP('172.16.1.13')
+            s.sendmail(msg['From'], msg['To'], msg.as_string())
+            s.quit()
+            context['alert']['level'] = "success"
+            context['alert']['message'] = "Successfully sent comment email. Thank you."
+        except Exception as e:
+            context['alert']['level'] = "danger"
+            context['alert']['message'] = "Unable to send contact email, please try again later. (%s)" % (e)
+        return render(request, 'ConvGCodeGenerator/index.html', context)
+    
+    return render(request, 'ConvGCodeGenerator/contact.html')
+    
